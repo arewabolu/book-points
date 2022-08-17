4e94c9b10182093875ef95e83cf15516f34bfeaf
arewabolu <bolu.arewa@gmail.com>
2022-08-17 18:53:18 +0100

    Moved titlefunc back to main

--- borrowed.go
+++ borrowed.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -17,16 +15,4 @@ func createIcon() *widget.Icon {
 
 func removeElementByIndex[T any](slice []T, index int) []T {
 	return append(slice[:index], slice[index+1:]...)
-}
-
-func titleEntry(text string) (*widget.Entry, binding.String) {
-	titleEntry := widget.NewEntry()
-	titleBind := binding.NewString()
-	titleEntry.SetText(text)
-	titleEntry.Resize(fyne.NewSize(250, 30))
-	titleEntry.OnChanged = func(s string) {
-		titleBind.Set(s)
-		titleEntry.Bind(titleBind)
-	}
-	return titleEntry, titleBind
 }
