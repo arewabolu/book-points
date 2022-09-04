335d7a5de3c75a24c3c4d69aef9b6d82ffdf5888
arewabolu <bolu.arewa@gmail.com>
2022-09-04 19:25:21 +0100

    used init to initialize basedir creation

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
 	"fyne.io/fyne/v2/dialog"
 )
 
+type window struct {
+	wind fyne.Window
+}
+
+func init() {
+	w := new(window)
+	err := makeDir()
+	if err != nil {
+		err = errors.New("unable to create new directory")
+		dialog.NewError(err, w.wind)
+	}
+}
+
 func main() {
 	app := app.New()
 	wind := app.NewWindow("BookTakes")
@@ -16,12 +29,6 @@ func main() {
 	wind.SetMaster()
 	//wind.SetFullScreen(true)
 	wind.Resize(fyne.NewSize(600, 600))
-
-	err := makeDir()
-	if err != nil {
-		err = errors.New("unable to create new directory")
-		dialog.NewError(err, wind)
-	}
 
 	fullWind := container.NewBorder(header(), nil, nil, nil, loadUI(wind))
 	wind.SetContent(fullWind)
